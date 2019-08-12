cdf13d2107efbe1b01e5d3c4275c2dee09ca8121
JoeyDeol <gurjothdeol@gmail.com>
2019-08-12 10:53:22 -0400

    Update typedoc configuration

--- typedoc.js
+++ typedoc.js
@@ -1,9 +1,7 @@
 module.exports = {
   "name": "SF-X",
-  "mode": "modules",
+  "mode": "file",
   "out": "./docs",
-  // the readme: none setting exposes a list of all packages as the docs landing page instead of the top level readme
-  "readme": "none",
   "ignoreCompilerErrors": true,
-  "exclude": ['**/test/**', '**/node_modules/**'],
+  "exclude": ['**/node_modules/**'],
 };
