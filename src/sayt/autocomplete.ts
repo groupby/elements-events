f4499422983a6a2dba30a14e093a8c6f4fbb0d3f
JoeyDeol <gurjothdeol@gmail.com>
2019-08-12 14:35:03 -0400

    Include the searchbox property within the autcomplete payload interfaces

--- src/sayt/autocomplete.ts
+++ src/sayt/autocomplete.ts
@@ -5,6 +5,8 @@ import { AutocompleteResultGroup } from './results';
 export const AUTOCOMPLETE_REQUEST = 'sfx::autocomplete_request';
 /** The type of the [[AUTOCOMPLETE_REQUEST]] event payload. */
 export interface AutocompleteRequestPayload extends QueryTimeAutocompleteConfig {
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
   /** The search term to autocomplete. */
   query: string;
 }
@@ -13,13 +15,18 @@ export interface AutocompleteRequestPayload extends QueryTimeAutocompleteConfig 
 export const AUTOCOMPLETE_RESPONSE = 'sfx::autocomplete_response';
 /** The type of the [[AUTOCOMPLETE_RESPONSE]] event payload. */
 export interface AutocompleteResponsePayload {
-  /** A list of autocomplete results. */
-  results: AutocompleteResultGroup[];
   /** The ID of the search box that initiated this request. */
   searchbox?: string;
+  /** A list of autocomplete results. */
+  results: AutocompleteResultGroup[];
 }
 
 /** The name of the event fired when an autocomplete request fails. */
 export const AUTOCOMPLETE_ERROR = 'sfx::autocomplete_error';
 /** The type of the [[AUTOCOMPLETE_ERROR]] event payload. */
-export type AutocompleteErrorPayload = Error;
+export interface AutocompleteErrorPayload {
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
+  /** The error thrown during this request. */
+  error: Error;
+};
