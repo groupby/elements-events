b3c787caa0b971e2a15a582528e398b8c1a5a48b
Johann Tutor <johann.tutor@groupbyinc.com>
2019-08-12 10:43:54 -0400

    WIP Add typedoc

---
+++ typedoc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "name": "SF-X",
+  "mode": "modules",
+  "out": "./docs",
+  // the readme: none setting exposes a list of all packages as the docs landing page instead of the top level readme
+  "readme": "none",
+  "ignoreCompilerErrors": true,
+  "exclude": ['**/test/**', '**/node_modules/**'],
+};
