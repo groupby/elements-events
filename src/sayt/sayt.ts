bd32e8b3540a4019c74424e8b743c02017090918
JoeyDeol <gurjothdeol@gmail.com>
2019-08-12 12:44:48 -0400

    Create SaytVisibilityPayload interface and update Sayt and Hide types to use it

--- src/sayt/sayt.ts
+++ src/sayt/sayt.ts
@@ -1,12 +1,20 @@
 /** The name of event fired when SAYT should be hidden. */
 export const SAYT_HIDE = 'sfx::sayt_hide';
 /** The type of the [[SAYT_HIDE]] event payload. */
-export interface SaytHidePayload {
-  searchbox?: string;
-};
+export type SaytHidePayload = SaytVisibilityPayload;
+
 /** The name of event fired when SAYT should be hidden. */
 export const SAYT_SHOW = 'sfx::sayt_show';
 /** The type of the [[SAYT_SHOW]] event payload. */
-export interface SaytShowPayload {
+export type SaytShowPayload = SaytVisibilityPayload;
+
+/**
+ * The base type of the SAYT visibility events.
+ *
+ * @see [[SaytHidePayload]]
+ * @see [[SaytShowPayload]]
+*/
+export interface SaytVisibilityPayload {
+  /** The searchbox ID associated with this event. */
   searchbox?: string;
 };
