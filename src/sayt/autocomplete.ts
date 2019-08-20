82f6676f2b673838099ba7a76e7d0c154d9eaea0
Johann Tutor <johann.tutor@groupbyinc.com>
2019-08-20 15:38:14 -0400

    Move searchbox below required properties

--- src/sayt/autocomplete.ts
+++ src/sayt/autocomplete.ts
@@ -5,28 +5,28 @@ import { AutocompleteResultGroup } from './results';
 export const AUTOCOMPLETE_REQUEST = 'sfx::autocomplete_request';
 /** The type of the [[AUTOCOMPLETE_REQUEST]] event payload. */
 export interface AutocompleteRequestPayload extends QueryTimeAutocompleteConfig {
-  /** The ID of the search box that initiated this request. */
-  searchbox?: string;
   /** The search term to autocomplete. */
   query: string;
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
 }
 
 /** The name of the event fired when the results of an autocomplete request have been received. */
 export const AUTOCOMPLETE_RESPONSE = 'sfx::autocomplete_response';
 /** The type of the [[AUTOCOMPLETE_RESPONSE]] event payload. */
 export interface AutocompleteResponsePayload {
-  /** The ID of the search box that initiated this request. */
-  searchbox?: string;
   /** A list of autocomplete results. */
   results: AutocompleteResultGroup[];
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
 }
 
 /** The name of the event fired when an autocomplete request fails. */
 export const AUTOCOMPLETE_ERROR = 'sfx::autocomplete_error';
 /** The type of the [[AUTOCOMPLETE_ERROR]] event payload. */
 export interface AutocompleteErrorPayload {
-  /** The ID of the search box that initiated this request. */
-  searchbox?: string;
   /** The error thrown during this request. */
   error: Error;
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
 };
--- src/search/search.ts
+++ src/search/search.ts
@@ -4,28 +4,28 @@ import { Product } from './product';
 export const SEARCH_REQUEST = 'sfx::search_request';
 /** The type of the [[SEARCH_REQUEST]] event payload. */
 export interface SearchRequestPayload {
- /** The ID of the search box that initiated this request. */
- searchbox?: string;
  /** The search term. */
  query: string;
+ /** The ID of the search box that initiated this request. */
+ searchbox?: string;
 }
 
 /** The name of the event fired when the results of a search request have been received.  */
 export const SEARCH_RESPONSE = 'sfx::search_response';
 /** The type of the [[SEARCH_RESPONSE]] event payload. */
 export interface SearchResponsePayload {
-  /** The ID of the search box that initiated this request. */
-  searchbox?: string;
   /** The products returned in the search response. */
   products: Product[];
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
 }
 
 /** The name of the event fired when a search request fails. */
 export const SEARCH_ERROR = 'sfx::search_error';
 /** The type of the [[SEARCH_ERROR]] event payload. */
 export interface SearchErrorPayload {
-  /** The ID of the search box that initiated this request. */
-  searchbox?: string;
   /** The error thrown during this request. */
   error: Error;
+  /** The ID of the search box that initiated this request. */
+  searchbox?: string;
 };
