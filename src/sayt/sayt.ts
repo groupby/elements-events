ae83357121798605ee45cba13d127927025ea1bc
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-19 15:20:33 -0400

    Change type alias to an interface
    Interfaces are preferred over type aliases where possible.

--- src/sayt/sayt.ts
+++ src/sayt/sayt.ts
@@ -3,12 +3,12 @@ import { WithSearchbox } from '../includes/searchbox';
 /** The name of the event fired when SAYT should be hidden. */
 export const SAYT_HIDE = 'sfx::sayt_hide';
 /** The type of the [[SAYT_HIDE]] event payload. */
-export type SaytHidePayload = SaytVisibilityPayload;
+export interface SaytHidePayload extends SaytVisibilityPayload {}
 
 /** The name of the event fired when SAYT should be shown. */
 export const SAYT_SHOW = 'sfx::sayt_show';
 /** The type of the [[SAYT_SHOW]] event payload. */
-export type SaytShowPayload = SaytVisibilityPayload;
+export interface SaytShowPayload extends SaytVisibilityPayload {}
 
 /**
  * The base type of the SAYT visibility events.
