6f2c5fc0d40667dc46672d39a992640091d56b63
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-20 12:15:00 -0400

    Extend all event payloads from WithGroup

--- src/sayt/autocomplete-list.ts
+++ src/sayt/autocomplete-list.ts
@@ -1,6 +1,6 @@
-import { WithQuery } from '../includes';
+import { WithQuery, WithGroup } from '../includes';
 
 /** The name of the event fired to change the active query term. */
 export const AUTOCOMPLETE_ACTIVE_TERM = 'sfx::autocomplete_active_term';
 /** The type of the [[AUTOCOMPLETE_ACTIVE_TERM]] event payload. */
-export interface AutocompleteActiveTerm extends WithQuery {}
+export interface AutocompleteActiveTerm extends WithQuery, WithGroup {}
