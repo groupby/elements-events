cef6e7bbad2b17516f3622297dd73fdcc39ee1f7
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-20 12:06:38 -0400

    Describe a missing group as equivalent to the empty string

--- src/includes/group.ts
+++ src/includes/group.ts
@@ -3,6 +3,9 @@
  * in some way with a particular search box.
  */
 export interface WithGroup {
-  /** The name of the group associated with this event. */
+  /**
+   * The name of the group associated with this event.
+   * A missing group should be treated as equivalent to the empty string.
+   */
   group?: string
 }
