129e2b764b786b84e269cb57cf2fd9a8a19dca8f
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-24 14:16:06 -0400

    SFX-248: Correctly build commit 2ac423d018b497e0eba45d1037248d0c5419d64d (#6)
    The files under includes were missing, and /product.* was not removed.

---
+++ dist/sayt/products.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** The name of the event fired to request SAYT products. */
+exports.SAYT_PRODUCTS_REQUEST = 'sfx::sayt_products_request';
+/** The name of the event fired when the results of a SAYT products request have been received. */
+exports.SAYT_PRODUCTS_RESPONSE = 'sfx::sayt_products_response';
+/** The name of the event fired when a SAYT products request fails. */
+exports.SAYT_PRODUCTS_ERROR = 'sfx::sayt_products_error';
+//# sourceMappingURL=products.js.map
