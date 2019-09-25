4dc48ec4ffbf64b867697a130b276471cc0d4e93
Johann Tutor <johann.tutor@groupbyinc.com>
2019-09-25 11:35:39 -0400

    Correct name of AutocompleteActiveTermPayload
    It was missing the Payload suffix.

--- src/sayt/autocomplete-list.ts
+++ src/sayt/autocomplete-list.ts
@@ -3,4 +3,4 @@ import { WithQuery, WithGroup } from '../includes';
 /** The name of the event fired to change the active query term. */
 export const AUTOCOMPLETE_ACTIVE_TERM = 'sfx::autocomplete_active_term';
 /** The type of the [[AUTOCOMPLETE_ACTIVE_TERM]] event payload. */
-export interface AutocompleteActiveTerm extends WithQuery, WithGroup {}
+export interface AutocompleteActiveTermPayload extends WithQuery, WithGroup {}
