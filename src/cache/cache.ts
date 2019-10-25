6039d195f7b6a19483d7084e11901dd5efbab15b
Johann Tutor <johann.tutor@groupbyinc.com>
2019-10-25 10:30:42 -0400

    Add cache response event

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -9,3 +9,13 @@ export interface CacheRequestPayload extends WithGroup {
   /** The name of the event under which to dispatch the cached data. */
   returnEvent: string;
 }
+
+/** The name of the event fired when data is returned from the cache. */
+export const CACHE_RESPONSE = 'sfx::cache_response';
+/** The type of the [[CACHE_RESPONSE]] event payload. */
+export interface CacheResponsePayload extends WithGroup {
+  /** The name of the cached data that was returned. */
+  name: string;
+  /** The data that was cached. */
+  data: string;
+}
