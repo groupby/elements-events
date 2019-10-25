c508034377472f636a4ffc90e32a922da358bbf1
Johann Tutor <johann.tutor@groupbyinc.com>
2019-10-25 15:34:58 -0400

    SFX-158: Add cache-related events (#15)
    New events CACHE_REQUEST, CACHE_RESPONSE and CACHE_ERROR have been
    added. The CACHE_RESPONSE name and CACHE_ERROR event are not currently
    expected to be used by SF-X but are added for consistency.

---
+++ src/cache/cache.ts
@@ -0,0 +1,26 @@
+import { ErrorPayload, WithGroup } from '../includes';
+
+/** The name of the event fired when data is being requested from the cache. */
+export const CACHE_REQUEST = 'sfx::cache_request';
+/** The type of the [[CACHE_REQUEST]] event payload. */
+export interface CacheRequestPayload extends WithGroup {
+  /** The name of the cached data to return. */
+  name: string;
+  /** The name of the event under which to dispatch the cached data. */
+  returnEvent: string;
+}
+
+/** The name of the event fired when data is returned from the cache. */
+export const CACHE_RESPONSE = 'sfx::cache_response';
+/** The type of the [[CACHE_RESPONSE]] event payload. */
+export interface CacheResponsePayload extends WithGroup {
+  /** The name of the cached data that was returned. */
+  name: string;
+  /** The data that was cached. */
+  data: string;
+}
+
+/** The name of the event fired when retrieving data from the cache failed. */
+export const CACHE_ERROR = 'sfx::cache_error';
+/** The type of the [[CACHE_ERROR]] event payload. */
+export interface CacheErrorPayload extends ErrorPayload, WithGroup {}
