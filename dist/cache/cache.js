1e6b3739237f9f4f553671b57485f5c0a6da55f5
Johann Tutor <johann.tutor@groupbyinc.com>
2019-11-01 16:00:53 -0400

    Build commit e74a6d01386ac3ac681d966671a721bd07b6007b (#16)

---
+++ dist/cache/cache.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** The name of the event fired when data is being requested from the cache. */
+exports.CACHE_REQUEST = 'sfx::cache_request';
+/** The recommended prefix to use for the name of the event fired when data is returned from the cache. */
+exports.CACHE_RESPONSE_PREFIX = 'sfx::cache_response-';
+/** The name of the event fired when retrieving data from the cache failed. */
+exports.CACHE_ERROR = 'sfx::cache_error';
+//# sourceMappingURL=cache.js.map
