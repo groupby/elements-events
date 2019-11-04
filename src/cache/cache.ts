5adb9c17294cf5b7d6ff15caadd25a2ce235bd96
Chantal <chandesrochers@gmail.com>
2019-11-04 16:21:20 -0500

    SFX-437: Rename project to "Elements" (#18)

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -1,7 +1,7 @@
 import { ErrorPayload, WithGroup } from '../includes';
 
 /** The name of the event fired when data is being requested from the cache. */
-export const CACHE_REQUEST = 'sfx::cache_request';
+export const CACHE_REQUEST = 'gbe::cache_request';
 /** The type of the [[CACHE_REQUEST]] event payload. */
 export interface CacheRequestPayload extends WithGroup {
   /** The name of the cached data to return. */
@@ -11,7 +11,7 @@ export interface CacheRequestPayload extends WithGroup {
 }
 
 /** The recommended prefix to use for the name of the event fired when data is returned from the cache. */
-export const CACHE_RESPONSE_PREFIX = 'sfx::cache_response-';
+export const CACHE_RESPONSE_PREFIX = 'gbe::cache_response-';
 /** The type of the cache response event payload. */
 export interface CacheResponsePayload extends WithGroup {
   /** The name of the cached data that was returned. */
@@ -21,6 +21,6 @@ export interface CacheResponsePayload extends WithGroup {
 }
 
 /** The name of the event fired when retrieving data from the cache failed. */
-export const CACHE_ERROR = 'sfx::cache_error';
+export const CACHE_ERROR = 'gbe::cache_error';
 /** The type of the [[CACHE_ERROR]] event payload. */
 export interface CacheErrorPayload extends ErrorPayload, WithGroup {}
--- src/sayt/autocomplete.ts
+++ src/sayt/autocomplete.ts
@@ -3,7 +3,7 @@ import { ErrorPayload, WithQuery, WithGroup } from '../includes';
 import { AutocompleteResultGroup } from './results';
 
 /** The name of the event fired to request autocomplete data. */
-export const AUTOCOMPLETE_REQUEST = 'sfx::autocomplete_request';
+export const AUTOCOMPLETE_REQUEST = 'gbe::autocomplete_request';
 /** The type of the [[AUTOCOMPLETE_REQUEST]] event payload. */
 export interface AutocompleteRequestPayload extends WithQuery, WithGroup {
   /** The extra configuration options to customize the request. */
@@ -11,7 +11,7 @@ export interface AutocompleteRequestPayload extends WithQuery, WithGroup {
 }
 
 /** The name of the event fired when the results of an autocomplete request have been received. */
-export const AUTOCOMPLETE_RESPONSE = 'sfx::autocomplete_response';
+export const AUTOCOMPLETE_RESPONSE = 'gbe::autocomplete_response';
 /** The type of the [[AUTOCOMPLETE_RESPONSE]] event payload. */
 export interface AutocompleteResponsePayload extends WithGroup {
   /** A list of autocomplete results. */
@@ -19,6 +19,6 @@ export interface AutocompleteResponsePayload extends WithGroup {
 }
 
 /** The name of the event fired when an autocomplete request fails. */
-export const AUTOCOMPLETE_ERROR = 'sfx::autocomplete_error';
+export const AUTOCOMPLETE_ERROR = 'gbe::autocomplete_error';
 /** The type of the [[AUTOCOMPLETE_ERROR]] event payload. */
 export interface AutocompleteErrorPayload extends ErrorPayload, WithGroup {}
