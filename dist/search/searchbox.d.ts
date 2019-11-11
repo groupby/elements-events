1696267a35450b9644d3bfa5cfc824bcb2bc8978
DanielMoniz-GBI <46679515+DanielMoniz-GBI@users.noreply.github.com>
2019-11-11 14:22:14 -0500

    SFX-423: Build commit (#22)

--- dist/search/searchbox.d.ts
+++ dist/search/searchbox.d.ts
@@ -5,6 +5,8 @@ export declare const UPDATE_SEARCH_TERM = "gbe::update_search_term";
 export interface UpdateSearchTermPayload extends WithGroup {
     /** A search term. */
     term: string;
+    /** Whether or not to trigger a search. */
+    search?: boolean;
 }
 /** The name of the event fired when the user has changed the text in the search box. */
 export declare const SEARCHBOX_INPUT = "gbe::searchbox_input";
