e8c5867a013ee2d6db088be798f899551c096f17
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-11-19 12:45:46 -0500

    Add import for SendableOrigin

--- src/beacons/beacons.ts
+++ src/beacons/beacons.ts
@@ -1,13 +1,14 @@
 import { Results } from 'groupby-api';
+import { SendableOrigin } from 'gb-tracker-client/models';
 
 /** The event to trigger a search beacon. */
-export const TrackerSearchEvent: string = 'gbe::tracker::search';
+export const TRACKER_SEARCH: string = 'gbe::tracker::search';
 
 /**
  * The type of the [[TrackerSearchEvent]] event payload.
  */
 export interface TrackerSearchPayload {
-  /** The search results. */
+  /** The search results from the API. */
   results: Results;
   /** The origin of the search action. */
   origin: SendableOrigin;
