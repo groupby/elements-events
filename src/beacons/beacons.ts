17fe36dac3f4d60b0baf29abca0c83b6d714feda
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-11-19 13:59:52 -0500

    Update docstring reference to event name

--- src/beacons/beacons.ts
+++ src/beacons/beacons.ts
@@ -5,7 +5,7 @@ import { SendableOrigin } from 'gb-tracker-client/models';
 export const TRACKER_SEARCH: string = 'gbe::tracker::search';
 
 /**
- * The type of the [[TrackerSearchEvent]] event payload.
+ * The type of the [[TRACKER_SEARCH]] event payload.
  */
 export interface TrackerSearchPayload {
   /** The search results from the API. */
