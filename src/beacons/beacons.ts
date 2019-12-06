d3d34f4c603576d2f55269c680785b5a832cef12
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-12-06 11:30:33 -0500

    Move docstring to be one line

--- src/beacons/beacons.ts
+++ src/beacons/beacons.ts
@@ -4,9 +4,7 @@ import { SendableOrigin } from 'gb-tracker-client/models';
 /** The event to trigger a search beacon. */
 export const TRACKER_SEARCH: string = 'gbe::tracker::search';
 
-/**
- * The type of the [[TRACKER_SEARCH]] event payload.
- */
+/** The type of the [[TRACKER_SEARCH]] event payload. */
 export interface TrackerSearchPayload {
   /** The search results from the API. */
   results: Results;
